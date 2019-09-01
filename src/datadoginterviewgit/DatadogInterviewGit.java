201e745322351dcba7a7a8854fb9c87f15f7196b
William Clarke <n00935124@ospreys.unf.edu>
2019-09-01 15:54:26 -0400

    First Push

---
+++ src/datadoginterviewgit/DatadogInterviewGit.java
@@ -0,0 +1,17 @@
+package datadoginterviewgit;
+
+/**
+ *
+ * @author LiamClarke
+ *  I created this project in order to Integrate my Github into the Datadog platform.
+ */
+public class DatadogInterviewGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
